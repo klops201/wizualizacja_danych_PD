a309fe098c17ac05fc11daf77bdca8d3558333b0
klops201 <73526787+klops201@users.noreply.github.com>
2021-05-10 11:32:52 +0200

    Add files via upload

---
+++ matplotlib.py
@@ -0,0 +1,21 @@
+#zad1.
+print('------zadanie 1------')
+
+xlsx = pd.ExcelFile('imiona.xlsx')
+df = pd.read_excel(xlsx)
+df1 = df.groupby(['Rok']).agg({'Liczba':['sum']})
+df1.plot()
+plt.show()
+print('\n\n')
+
+#zad2.
+print('------zadanie 2------')
+df2 = df.groupby(['Plec']).agg({'Liczba':['sum']})
+
+#print(df2)
+w1 = df2.plot.bar()
+
+plt.xticks(rotation=0)
+plt.title('suma urodzen M i K')
+plt.show()
+print('\n\n')
